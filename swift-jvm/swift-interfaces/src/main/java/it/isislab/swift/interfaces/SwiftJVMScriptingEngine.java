40cd32ab865730cfe1372dd6be00b449f58f41ce
Carmine Spagnuolo <spagnuolocarmine@gmail.com>
2016-07-07 10:41:59 +0200

    change eval method

--- swift-jvm/swift-interfaces/src/main/java/it/isislab/swift/interfaces/SwiftJVMScriptingEngine.java
+++ swift-jvm/swift-interfaces/src/main/java/it/isislab/swift/interfaces/SwiftJVMScriptingEngine.java
@@ -77,5 +77,50 @@ public class SwiftJVMScriptingEngine {
 		
 
 	}
+	public static String eval(String engine_name_given,String code)
+	{
+		Object output=null;
+		StringWriter writer;
+		ScriptContext context;
+		engine_name=engine_name_given;
+		try {
+
+			switch (engine_name) {
+			case SwiftJVMScriptingEngineNames.CLOJURE:
+				engine = new ClojureScriptEngine();
+				output=(engine.eval(code,engine.getContext())).toString();
+				return output!=null?output.toString():"";
+			case SwiftJVMScriptingEngineNames.GROOVY:
+				engine = new ScriptEngineManager().getEngineByName(SwiftJVMScriptingEngineNames.GROOVY);
+				 writer = new StringWriter();
+				 context = engine.getContext();
+				context.setWriter(writer);
+				engine.eval(code);
+				output = writer.toString();
+
+				return output!=null?(String)output:"";
+			case SwiftJVMScriptingEngineNames.SCALA:
+				engine = new ScalaScriptEngine();
+				output=engine.eval(code);
+				return output!=null?output.toString():"";
+			case SwiftJVMScriptingEngineNames.JAVASCRIPT:
+				engine = new ScriptEngineManager().getEngineByName(SwiftJVMScriptingEngineNames.JAVASCRIPT);
+				 writer = new StringWriter();
+				 context = engine.getContext();
+				context.setWriter(writer);
+				engine.eval(code);
+				output = writer.toString();
+
+				return output!=null?(String)output:"";
+			default:
+				return null;
+			}
+		} catch (ScriptException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
 
 }
--- swift-jvm/swift-interfaces/src/test/java/it/isislab/swift/interfaces/Tests.java
+++ swift-jvm/swift-interfaces/src/test/java/it/isislab/swift/interfaces/Tests.java
@@ -8,97 +8,92 @@ import junit.framework.TestSuite;
  * Unit test for simple App.
  */
 public class Tests 
-    extends TestCase
+extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public Tests( String testName )
-    {
-        super( testName );
-    }
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	public Tests( String testName )
+	{
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( Tests.class );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite()
+	{
+		return new TestSuite( Tests.class );
+	}
 
-    /**
-     */
-    public void testClojure()
-    {
-    	
-    	SwiftJVMScriptingEngine.setEngine(SwiftJVMScriptingEngineNames.CLOJURE);
-		String script_hello_clojure=""
-			+"	(ns clojure.examples.hello                          \n"
-			+"			   (:gen-class))                            \n"
-            +"                                                      \n"
-			+"			(defn hello-world [username]                \n"
-			+"			   (str (format \"Hello, %s\" username))) \n"
-            +"                                                      \n"
-			+"			(hello-world \"Red\")                       \n"
-			+"";
-    	System.out.println(SwiftJVMScriptingEngine.eval(script_hello_clojure));
-        
-    }
-    /**
-     */
-    public void testGroovy()
-    {
-    	
-    	SwiftJVMScriptingEngine.setEngine(SwiftJVMScriptingEngineNames.GROOVY);
-    	
-    	String script_fac_groovy= ""
+	/**
+	 */
+	public  void testClojure()
+	{
+			String script_hello_clojure=""
+					+"	(ns clojure.examples.hello                          \n"
+					+"			   (:gen-class))                            \n"
+					+"                                                      \n"
+					+"			(defn hello-world [username]                \n"
+					+"			   (str (format \"Hello, %s\" username))) \n"
+					+"                                                      \n"
+					+"			(hello-world \"Red\")                       \n"
+					+"";
+			System.out.println(SwiftJVMScriptingEngine.eval(SwiftJVMScriptingEngineNames.CLOJURE,script_hello_clojure));
+
+	}
+	/**
+	 */
+	public synchronized void testGroovy()
+	{
+
+
+		String script_fac_groovy= ""
 				+"import java.security.MessageDigest "
 				+ " \n"
 				+ "def hash(text){"
 				+ " MessageDigest"
 				+ ".getInstance(\"SHA-512\")"
-						+ ".digest(text.getBytes(\"UTF-8\"))"
-							+".encodeBase64().toString()"
-							+"} \n"
-							+ "\n println hash(UUID.randomUUID().toString())"
+				+ ".digest(text.getBytes(\"UTF-8\"))"
+				+".encodeBase64().toString()"
+				+"} \n"
+				+ "\n println hash(UUID.randomUUID().toString())"
 				+" ";
-    	System.out.println(SwiftJVMScriptingEngine.eval(script_fac_groovy));
-    	
-    
-    	
-        
-    }
-    //TODO ADD RETURN VALUE STRING
-    public void testScala()
-    {
-    	
-    	SwiftJVMScriptingEngine.setEngine(SwiftJVMScriptingEngineNames.SCALA);
-    	
-    	String script_scala = ""
+		System.out.println(SwiftJVMScriptingEngine.eval(SwiftJVMScriptingEngineNames.GROOVY,script_fac_groovy));
+
+
+
+
+	}
+	//TODO ADD RETURN VALUE STRING
+	public synchronized void testScala()
+	{
+
+
+		String script_scala = ""
 				+"object HelloWorld {\n"
 				+"def main(args: Array[String]) = println(\"Hello Scala!\")\n"
 				+"}\n"
 				+"";
-    	System.out.println(SwiftJVMScriptingEngine.eval(script_scala));
-    	
-        
-    }
-    //TODO ADD RETURN VALUE STRING
-    public void testJavaScript()
-    {
-    	
-    	SwiftJVMScriptingEngine.setEngine(SwiftJVMScriptingEngineNames.JAVASCRIPT);
-    	
-    	String script_js = ""
-    		+"	function test() {                                                                                                  \n"            
-    		+"  var x = 2, y = 4;                                                                                                  \n"
-    		+"  return eval(\"x + y\");  // Direct call, uses local scope, result is 6                                         \n"
-    		+"}    test();  ";
-    	
-    	System.out.println(SwiftJVMScriptingEngine.eval(script_js));
-    	
-        
-    }
+		System.out.println(SwiftJVMScriptingEngine.eval(SwiftJVMScriptingEngineNames.SCALA,script_scala));
+
+
+	}
+	//TODO ADD RETURN VALUE STRING
+	public synchronized void testJavaScript()
+	{
+
+
+		String script_js = ""
+				+"	function test() {                                                                                                  \n"            
+				+"  var x = 2, y = 4;                                                                                                  \n"
+				+"  return eval(\"x + y\");  // Direct call, uses local scope, result is 6                                         \n"
+				+"}    test();  ";
+
+		System.out.println(SwiftJVMScriptingEngine.eval(SwiftJVMScriptingEngineNames.JAVASCRIPT,script_js));
+
+
+	}
 }
